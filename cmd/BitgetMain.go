4374774f0f42ae2227360774a15c221345e0cf40
admin <admin@btcmdeMacBook-Pro.local>
2025-01-23 15:05:55 +0800

    add config

--- cmd/BitgetMain.go
+++ cmd/BitgetMain.go
@@ -3,16 +3,28 @@ package main
 import (
 	"fmt"
 
+	"github.com/Shuixingchen/bitget-golang-sdk-api/config"
+	"github.com/Shuixingchen/bitget-golang-sdk-api/constants"
 	"github.com/Shuixingchen/bitget-golang-sdk-api/internal/model"
 	"github.com/Shuixingchen/bitget-golang-sdk-api/pkg/client/ws"
 )
+
+var conf = config.Config{
+	BaseUrl:       "https://api.bitget.com",
+	WsUrl:         "wss://ws.bitget.com/mix/v1/stream",
+	ApiKey:        "",
+	SecretKey:     "",
+	PASSPHRASE:    "",
+	TimeoutSecond: 30,
+	SignType:      constants.SHA256,
+}
 
 func main() {
 	client := new(ws.BitgetWsClient).Init(true, func(message string) {
 		fmt.Println("default error:" + message)
 	}, func(message string) {
 		fmt.Println("default error:" + message)
-	})
+	}, conf)
 
 	var channelsDef []model.SubscribeReq
 	subReqDef1 := model.SubscribeReq{
